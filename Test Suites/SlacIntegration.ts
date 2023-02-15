48618ac4988d795097b1c5c6405e9edb9c081206
Rahul Kumar <merahul220@gmail.com>
2023-02-15 20:01:20 +0530

    made changes in code

---
+++ Test Suites/SlacIntegration.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SlacIntegration</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>665c6fc0-1a80-476c-81c5-49b82ce1e15e</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef910e2f-1c05-4086-8cd8-83d04d57fd16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_Slac_IntergrationTesting</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
